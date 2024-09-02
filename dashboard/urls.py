9ee44bb26b85efa8c52f20394f94a0677190bfe1
JaredAhaza <jaredahazq@gmail.com>
2024-09-02 22:22:37 +0300

    updated page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('student_dashboard', views.student_dashboard, name='student_dashboard'),
     path('update_student_profile', views.update_student_profile, name='update_student_profile'),
-    path('student_profile', views.student_profile, name='student_profile'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,3 @@ def student_dashboard(request):
 
 def update_student_profile(request):
     return render(request, 'dashboard/update_student_profile.html')
-
-def student_profile(request):
-    return render(request, 'dashboard/student_profile.html')
