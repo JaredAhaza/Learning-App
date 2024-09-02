d0a81b416e1cdf509130f391acd994e6f36f77b5
JaredAhaza <jaredahazq@gmail.com>
2024-09-02 22:19:24 +0300

    updated page

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def student_dashboard(request):
     return render(request, 'dashboard/student_dashboard.html')
+
+def update_student_profile(request):
+    return render(request, 'dashboard/update_student_profile.html')
+
+def student_profile(request):
+    return render(request, 'dashboard/student_profile.html')
