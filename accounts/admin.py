90a0935c7b230c8d833bf4c4b79e9bbdc747ecbb
JaredAhaza <jaredahazq@gmail.com>
2024-10-01 14:30:44 +0300

    Registered the yteacher and teacherprofile to th e admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -17,3 +17,19 @@ class StudentProfileAdmin(admin.ModelAdmin):
                 pass
         super().save_model(request, obj, form, change)
 admin.site.register(StudentProfile, StudentProfileAdmin)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Teacher, TeacherAdmin)
+
+class TeacherProfileAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not obj.name:
+            try:
+                teacher = Student.objects.get(user=request.user)
+                obj.name = teacher.user.get__full__name()
+            except Teacher.DoesNotExist:
+                pass    
+        super().save_model(request, obj, form, change)
+admin.site.register(TeacherProfile, TeacherProfileAdmin)
