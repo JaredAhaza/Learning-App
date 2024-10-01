c006f766d6f91ba0d8fc4a7b6851851853367aa5
JaredAhaza <jaredahazq@gmail.com>
2024-10-01 14:23:00 +0300

    Created my teacher and teacherprofile model together with their forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, Permission
 from django.contrib.auth.forms import UserCreationForm
 from . import models
 from django.core.exceptions import ValidationError
 from django.forms import DateInput
+from django.contrib.contenttypes.models import ContentType
 
 class StudentUserForm(UserCreationForm):
     email = forms.EmailField()
@@ -34,9 +35,37 @@ class SuperUserCreationForm(UserCreationForm):
         user = super().save(commit=False)
         user.is_superuser = True
         user.save()
-        group = Group.objects.get(name='STUDENT')
-        group.user_set.remove(user)
-        return user
+        superuser_group = Group.objects.get_or_create(name='SUPERUSER')
+        user.groups.add(superuser_group)
+
+        view_student_permission, created = Permission.objects.get_or_create(
+            codename='view_student',
+            name = 'Can view student group',
+            content_type = ContentType.objects.get_for_model(Group)
+        )
+
+        manage_student_permission, created = Permission.objects.get_or_create(
+            codename='manage_student',
+            name = 'Can manage student group',
+            content_type = ContentType.objects.get_for_model(Group)
+        )
+
+        view_steacher_permission, created = Permission.objects.get_or_create(
+            codename='view_teacher',
+            name = 'Can view teacher group',
+            content_type = ContentType.objects.get_for_model(Group)
+        )
+
+        manage_teacher_permission, created = Permission.objects.get_or_create(
+            codename='manage_teacher',
+            name = 'Can manage teacher group',
+            content_type = ContentType.objects.get_for_model(Group)
+        )
+
+        superuser_group.permissions.add(view_student_permission)
+        superuser_group.permissions.add(manage_student_permission)
+        superuser_group.permissions.add(view_steacher_permission)
+        superuser_group.permissions.add(manage_teacher_permission)
 
 class StudentForm(forms.ModelForm):
     class Meta:
@@ -59,5 +88,45 @@ class StudentProfileForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['profile_picture'].required = False
 
+
+class TeacherUserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists")
+        return email
     
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.save()
+        group = Group.objects.get(name='TEACHER')
+        user.groups.add(group)
+        return user
     
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = models.Teacher
+        fields = [ 'email']
+
+
+class TeacherProfileForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=DateInput(format='%Y-%m-%d'))  # Use consistent format
+
+    class Meta:
+        model = models.TeacherProfile
+        fields = ['date_of_birth', 'phone_number', 'bio', 'profile_picture', 'gender']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'class': 'input--style-4 js-datepicker', 'type': 'text', 'placeholder': 'YYYY-MM-DD'}),
+            'gender': forms.RadioSelect
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['profile_picture'].required = False
--- accounts/models.py
+++ accounts/models.py
@@ -22,7 +22,6 @@ class Student(models.Model):
     def __str__(self):
         return f"{self.user.username}"
 
-
 class StudentProfile(models.Model):
     Gender = (
         ('Male', 'M'),
@@ -39,3 +38,40 @@ class StudentProfile(models.Model):
     
     def __str__(self):
         return f"Student Profile for {self.student.user}"
+    
+class Teacher(models.Model):
+    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher')
+    groups = models.ManyToManyField(User, related_name='teacher_groups')
+    user_permissions = models.ManyToManyField(User, related_name='teacher_permissions')
+    teacher_id = models.CharField(max_length=3, primary_key=True)
+
+    def save(self, *args, **kwargs):
+        if not self.teacher_id:
+            last_teacher = Teacher.objects.order_by('teacher_id').last()
+            if last_teacher:
+                next_id = int(last_teacher.teacher_id[2:]) + 1 
+            else:
+                next_id = 1
+            self.teacher_id = f"TL{next_id:03d}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username}"
+    
+
+class TeacherProfile(models.Model):
+    Gender = (
+        ('Male', 'M'),
+        ('Female', 'F'),
+        ('Rather not say', 'RNS'),
+    )
+    teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    gender = models.CharField(blank=True, choices=Gender, max_length=20)
+
+    def __str__(self):
+        return f"Teacher Profile for {self.teacher.user}"
